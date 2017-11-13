f6517fbcb64590beb277aa34c6212905ed8c9756
Eric Lau <yasuyo369@gmail.com>
2017-11-13 09:02:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ var playlist = {Sinatra: "My Way"}
 
 function removeFromPlaylist(playlist, artistName)
   {
-    delete playlist.artistName;
-    return playlist;
+    var newPlaylist = delete playlist.artistName;
+    return newPlaylist;
   }
