9116c524c1c94cdf8afc480d821092a9c7fb0f29
Eric Lau <yasuyo369@gmail.com>
2017-11-13 08:59:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,3 +5,9 @@ function updatePlaylist(playlist, artistName, songTitle)
     playlist.artistName = songTitle;
     return playlist;
   }
+
+function removeFromPlayList(playlist, artistName)
+  {
+    delete playlist.artistName;
+    return playlist;
+  }
